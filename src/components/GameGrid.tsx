635edeb45754b46a555b6ff3fc202614eb92fd5a
prabhu-raja <prabhuraja@gmail.com>
2023-05-09 10:09:45 -0400

    24 - Build Platform Selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={10} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={3}>
         {isLoading &&
           skeletons.map((skl) => (
             <GameCardContainer key={skl}>
