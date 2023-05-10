c589deede845a84701afbb5c7a6a0fdb2543c3e6
prabhu-raja <prabhuraja@gmail.com>
2023-05-10 13:57:12 -0400

    29 - Fix for no image scenario

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,11 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
-  const target = 'media/'
+  if (!url) return noImage;
+  const target = 'media/';
   const index = url.indexOf(target) + target.length;
   const newUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
   return newUrl;
-}
+};
 
 export default getCroppedImageUrl;
